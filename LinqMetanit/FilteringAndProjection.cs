d5423350fe93a085f57680941726395f6df5d8a1
Gregor812 <juicefruit89@gmail.com>
2018-02-24 13:38:12 +0300

    Last changes

--- LinqMetanit/FilteringAndProjection.cs
+++ LinqMetanit/FilteringAndProjection.cs
@@ -208,26 +208,4 @@ namespace LinqMetanit
                 Console.WriteLine("{0} - {1}", p.Name, p.Phone);
         }
     }
-
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public List<string> Languages { get; set; }
-        public User()
-        {
-            Languages = new List<string>();
-        }
-
-        public override string ToString()
-        {
-            return $"{Name} - {Age}";
-        }
-    }
-
-    public class Phone
-    {
-        public string Name { get; set; }
-        public string Company { get; set; }
-    }
 }
--- LinqMetanit/JoinGroupJoinAndZip.cs
+++ LinqMetanit/JoinGroupJoinAndZip.cs
@@ -10,22 +10,104 @@ namespace LinqMetanit
     {
         public static void Example1()
         {
+            var teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country = "Германия" },
+                new Team { Name = "Барселона", Country = "Испания" }
+            };
 
+            var players = new List<Player>()
+            {
+                new Player { Name = "Месси", Team = "Барселона"},
+                new Player { Name = "Неймар", Team = "Барселона"},
+                new Player { Name = "Роббен", Team = "Бавария"}
+            };
+
+            var result = from pl in players
+                         join t in teams on pl.Team equals t.Name
+                         select new { pl.Name, pl.Team, t.Country };
+
+            foreach (var r in result)
+                Console.WriteLine("{0} - {1} ({2})", r.Name, r.Team, r.Country);
         }
 
         public static void Example2()
         {
+            var teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country = "Германия" },
+                new Team { Name = "Барселона", Country = "Испания" }
+            };
 
+            var players = new List<Player>()
+            {
+                new Player { Name = "Месси", Team = "Барселона"},
+                new Player { Name = "Неймар", Team = "Барселона"},
+                new Player { Name = "Роббен", Team = "Бавария"}
+            };
+
+            var result = players.Join(teams, p => p.Team, t => t.Name, (p, t) => new { p.Name, p.Team, t.Country });
+
+            foreach (var r in result)
+                Console.WriteLine("{0} - {1} ({2})", r.Name, r.Team, r.Country);
         }
 
         public static void Example3()
         {
+            var teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country = "Германия" },
+                new Team { Name = "Барселона", Country = "Испания" }
+            };
 
+            var players = new List<Player>()
+            {
+                new Player { Name = "Месси", Team = "Барселона"},
+                new Player { Name = "Неймар", Team = "Барселона"},
+                new Player { Name = "Роббен", Team = "Бавария"}
+            };
+
+            var result = teams.GroupJoin(players,
+                                         t => t.Name,
+                                         p => p.Team,
+                                         (team, pls) => new
+                                         {
+                                             team.Name,
+                                             team.Country,
+                                             Players = pls.Select(p => p.Name)
+                                         });
+
+            foreach (var g in result)
+            {
+                Console.WriteLine("{0} ({1})", g.Name, g.Country);
+
+                foreach (var p in g.Players)
+                    Console.WriteLine(p);
+
+                Console.WriteLine();
+            }
         }
 
         public static void Example4()
         {
+            var teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country ="Германия" },
+                new Team { Name = "Барселона", Country ="Испания" },
+                new Team { Name = "Ювентус", Country ="Италия" }
+            };
 
+            var players = new List<Player>()
+            {
+                new Player {Name="Роббен", Team="Бавария"},
+                new Player {Name="Неймар", Team="Барселона"},
+                new Player {Name="Буффон", Team="Ювентус"}
+            };
+
+            var result = players.Zip(teams, (p, t) => new { p.Name, p.Team, t.Country });
+
+            foreach (var r in result)
+                Console.WriteLine("{0} - {1} ({2})", r.Name, r.Team, r.Country);
         }
     }
 }
